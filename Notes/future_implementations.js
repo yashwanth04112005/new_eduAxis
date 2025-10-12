2b36851ab9fbf3428b78c8022b22a8cb541ba492
yashwanth04112005 <paladugulayashwanth2005@gmail.com>
2025-10-12 22:41:46 +0530

    update future

--- Notes/future_implementations.js
+++ Notes/future_implementations.js
@@ -4,11 +4,7 @@
 // add recent payments viewing in admin dashboard
 // add payment history for students
 // add an option for students to pay the amount
-<<<<<<< HEAD
- 
-=======
 // signout button in dashboard
 //teacher deleting assignments
 // reject leave in admin
 // view assignment submissions in teacher dashboard
->>>>>>> 532abd5cb9699328c4e1246bfef1b6fe89be071c
